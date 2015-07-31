11d513dd3882deb494f1fccdfebc33ee25cdd8b8
Raj Krishnan R <rkrishnan1993@live.com>
2015-07-31 11:22:03 +0530

    Download PAge

---
+++ dev/index.php
@@ -0,0 +1,10 @@
+<?php
+if(isset($_GET['db_schema']))
+{
+header('Content-Type: text/sql');
+header('Content-Disposition: attachment; filename=notes_db_schema.sql');
+header('Pragma: no-cache');
+readfile("notes_db_schema.sql");
+}
+echo '<p>The Database scheme will be downloaded automatically as a ".sql" file extension , incase if not downloaded <a href="notes_db_schema.sql"> click here</a>';
+?>
