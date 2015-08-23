748731fa288c1ccfaf0fb259a95fe1ac3233d5a7
Raj Krishnan R <rkrishnan1993@live.com>
2015-08-23 12:26:05 +0530

    Links are now displayed. Crawling not emdedd

--- mylinks.php
+++ mylinks.php
@@ -1,16 +1,32 @@
 <html>
 <head>
     <script src="ajax_1_10_2.js"></script>
+      <script src="lib/jquery-ui.js"></script>
+
 <title>You Have to Bring It Back</title>
 <style>
     .each
     {
 position:relative;
-        float: bottom;
-        margin-top: : 1%;
+        margin-top:  1%;
         color:#ff0;
-        background:#0f0;
+background-image: url(images/tile.png);
         margin-top:1%;
+        min-height :50px;
+        overflow-x:auto;
+        width:100%;
+    }
+    .each h1
+    {
+        color:#485465;
+        font-size: 20px;
+        text-align: center;
+        font-family: arial,serif;
+    }
+    .each .link
+    {
+        font-family: arial,serif;
+        font-size: 15px;
         
     }
     </style>
@@ -18,25 +34,41 @@ position:relative;
     
     </script>
 </head>
-<body onload="fetch(10,0);">
+<body onload="fetch();">
     <div id="container">
 <script>
-    function fetch(count,offset)
+    var count=10,offset=-10;
+     $(window).scroll(function() { 
+   if($(window).scrollTop() + $(window).height() != $(document).height()) {
+       fetch();
+   }
+   });
+    function fetch()
     {
+     offset+=10;   
         $.get('links.php?limit='+count+','+offset,function(data,success)
     {
-        console.log(data);
         var json=JSON.parse(data);
         var count=0;
-        while(typeof(json[count].id))
+        console.log(json[count].id);
+        while(json[count].id!==null)
             {
         var div = document.createElement('div');
 document.getElementById('container').appendChild(div).setAttribute('class',"each");
 document.getElementById('container').appendChild(div).setAttribute('id',count);
-
-        document.getElementById(count).innerHTML=json[count].url;
+if(json[count].url.length>30)
+{
+    var d_url= json[count].url.substr(0,30)+'...';
+}
+else
+    {
+        var d_url=json[count].url;
+    }
+            document.getElementById(count).innerHTML='<h1>This a title</h1><span class="link"><a target="_new" href="'+json[count].url+'">'+d_url+'</a><span>';
         count++;
-            }
+}
+
+            
     });
     }
     </script>
