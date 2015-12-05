ff92f6e2f3d335b558d647699193bc9ebe0137a0
Raj Krishnan R <rkrishnan1993@live.com>
2015-12-05 20:48:21 +0530

    mobile redirectionb

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
+require_once 'Mobile_Detect.php';
+$detect = new Mobile_Detect;
+if ( $detect->isMobile() ) {
+ header('location: /way');
+}
+else
+{
+     header('location: way');
 
+}
 if(isset($_COOKIE['email']))
 {
     header('location: login.php?cook');
