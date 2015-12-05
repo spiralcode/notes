16bea8da2c06a21c3712dd7fb5d35286f54ef2d9
Raj Krishnan R <rkrishnan1993@live.com>
2015-12-05 15:04:49 +0530

    error check

--- way/index.php
+++ way/index.php
@@ -1,9 +1,5 @@
 <!doctype html>
 <html class="no-js" lang="">
-    <?php
-    echo "Manassin
-        ";
-    ?>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
