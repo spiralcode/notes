6f322df7c299bdaf2f1d5fa9bd50e8179653c614
Raj Krishnan R <rkrishnan1993@live.com>
2016-04-17 12:38:46 +0530

    file stuff

---
+++ elements.php
@@ -0,0 +1,27 @@
+<?php 
+function mimeType($filename)
+{
+    $part = explode(".",$filename);
+    $ext = $part[sizeof($part)-1];
+    if($ext=='mp3')
+    return "audio/mpeg";
+    elseif($ext=="mp4")
+    return "video/mp4";
+    elseif($ext=="mp4a")
+    return "audio/mp4";
+        elseif($ext=="html")
+    return "text/html";
+            elseif($ext=="pdf")
+    return "application/pdf";
+             elseif($ext=="png")
+    return "image/png";
+             elseif($ext=="jpeg")
+    return "image/jpeg";
+            elseif($ext=="jpg")
+    return "image/jpg";
+             elseif($ext=="gif")
+    return "image/gif";
+    else
+    return "text/plain";
+}
+?>
--- redirectToFile.php
+++ redirectToFile.php
@@ -2,6 +2,7 @@
 include 'connect.php';
 include 'session_check.php';
 include 'ease.php';
+include 'elements.php';
 if($_SERVER['HTTP_HOST']!='localhost')
 {
 $target_dir=$_ENV['OPENSHIFT_DATA_DIR']."/";
@@ -17,7 +18,33 @@ $query=mysqli_query($link,"select * from image where id = $pid and userid = $use
             $filename=$data['filename'];
         }
 $file = $target_dir.$filename;
- header('Location: '.$file);
+
+set_time_limit(0);
+$filePath = $file;
+$strContext=stream_context_create(
+    array(
+        'http'=>array(
+        'method'=>'GET',
+        'header'=>"Accept-language: en\r\n"
+        )
+    )
+);
+$fpOrigin=fopen($filePath, 'rb', false, $strContext);
+header('Content-Disposition: inline; filename="'.$filename.'"');
+header('Pragma: no-cache');
+//header('Content-type: '+mime_content_type($file));
+header('Content-type: '.mimeType($filename));
+header('Content-Length: '.filesize($filePath));
+while(!feof($fpOrigin)){
+  $buffer=fread($fpOrigin, 4096);
+  echo $buffer;
+  flush();
+}
+fclose($fpOrigin);
+
+
+
+// header('Location: '.$file);
 
 
 ?>
