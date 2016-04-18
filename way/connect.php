3980c185e04058b1669519cea43c3193c0009043
Raj Krishnan R <rkrishnan1993@live.com>
2016-04-18 16:08:43 +0530

    v change

---
+++ way/connect.php
@@ -0,0 +1,24 @@
+<?php    
+if($_SERVER['HTTP_HOST']!='localhost')
+{
+   //Dont alter it's remote 
+$host = "127.10.171.2:3306";
+$user = "adminz8hImgI";
+$pass = 'rUP7aW8my2r6';
+$db = 'note';
+}   
+else
+{
+//For localhost
+$host = "localhost";
+$user = "root";
+$pass = 'dbase001';
+$db = 'notes';	
+}
+$conn = new mysqli($host,$user,$pass);
+$link=$conn;
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+} 
+$db=mysqli_select_db($conn,$db);
+?>
