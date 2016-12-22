ddfd11b080d4df81bb777eaea477ccea229a669e
Your Name <you@example.com>
2016-12-22 18:38:07 +0530

    hello

--- draw/add.php
+++ draw/add.php
@@ -5,7 +5,7 @@ include '../connect.php';
 ?>
 <html>
 <head>
-<title>Notes Draw</title>
+<title>Notes FOE</title>
 
   <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
--- draw/index.php
+++ draw/index.php
@@ -9,7 +9,7 @@ include '../connect.php';
 ?>
 <html>
 <head>
-<title>Notes Draw</title>
+<title>Notes FOE</title>
   <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
           <link rel="stylesheet" href="notesGen.css">
