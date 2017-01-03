19a05cba89be359319e6a831e880c948ee34ea0f
Your Name <you@example.com>
2017-01-03 23:29:35 +0530

    Th

--- special/birthDay/index.php
+++ special/birthDay/index.php
@@ -28,7 +28,7 @@
 </div>
 <div style="text-align:center">
 <p><span id="opC">" </span>
-Count, this is the first one !.  And I'm overwhelmed, of this Love, which makes us to spend the rest of all our Birthdays together. After some years, we would have so many birthdays to celeberate, of our Kids, but then also there will be someone, who loves me a lot and whom I love, who's gonna get all my kisses in her birthday.  
+Count, this is the first one !.  And I'm overwhelmed, of this Love, which would make us to spend the rest of all our Birthdays together. After some years, we would have so many birthdays to celeberate, of our Kids, but then also there will be you, who loves me lot and whom I love, who's gonna get all my kisses in her birthday.  
 <span id="opc"> "</span></p>
   </div>
 </body>
