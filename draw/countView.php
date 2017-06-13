8dc32a610ba17cf7c26339ea84273eccd47a3257
Your Name <you@example.com>
2017-06-13 11:05:45 +0530

    patch - counter

--- draw/countView.php
+++ draw/countView.php
@@ -1,15 +1,18 @@
 <?php
-session_start();
 include '../connect.php';
-$q = mysqli_query($link,"select * from draw_view_count ") or die(mysqli_error($link));
+
+//session_start();
+$qq = mysqli_query($link,"select * from draw_view_count ") or die(mysqli_error($link));
 $ip = "";
-while($r=mysqli_fetch_array($q))
+$act=0;
+while($r = mysqli_fetch_array($qq))
 {
     $loop = $r['count'];
     $did = $r['id'];
 for($a = 0;$a<$loop;$a++)
 {
-$q = mysqli_query($link,"insert into view_count values ($did,'$ip',now()");
+    echo "Activity : ".$act."\n";
+mysqli_query($link,"insert into view_count values ($did,'$ip',now())") or die (mysqli_error($link));
 }
 }
 ?>
