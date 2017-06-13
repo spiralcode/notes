c96a82369468824e89808571340c67a17774c0ee
Your Name <you@example.com>
2017-06-13 11:42:22 +0530

    real ip fetch

--- draw/countViewNew.php
+++ draw/countViewNew.php
@@ -2,7 +2,8 @@
 session_start();
 include '../connect.php';
 $did = $_GET['id'];
-$ip = $_SERVER['REMOTE_ADDR'];
+
+$ip = $getRealIpAddr();
 $q = mysqli_query($link,"select * from draw where id = $did") or die(mysqli_error($link));
 if(isset($_SESSION['userid']))
 {
@@ -19,5 +20,20 @@ else
     $q = mysqli_query($conn,"insert into view_count values ($did,'$ip',now())") or die(mysqli_error($link));
 
 }
-
+function getRealIpAddr()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+    {
+      $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+    {
+      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else
+    {
+      $ip=$_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
 ?>
