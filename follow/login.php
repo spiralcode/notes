0e7dd9f2816d413ceae6b5b7a06e389da4f2e18b
Your Name <you@example.com>
2017-07-16 15:21:52 +0530

    Notes Follow Introduces

--- follow/login.php
+++ follow/login.php
@@ -2,11 +2,10 @@
 include 'connect.php';
 $userid = mysqli_real_escape_string($link,$_POST['userid']);
 $pass = mysqli_real_escape_string($link,$_POST['pass']);
-echo "Reached : "+$userid;
 class user{
 	var $title;
 	var $id;
-	function setTitle($title)
+	public function setTitle($title)
 	{
 		$this->title=$title;
 		
@@ -23,8 +22,9 @@ class user{
 $resultArray = array();
 
 
+$q = mysqli_query($link,"select * from follow_profiles where userid = '$userid' and password = '$pass'" ) or die(mysqli_error($link));
 
-$q = mysqli_query($link,"select * from follow_profiles where userid = $userid and password = $pass" ) or die(mysqli_error($link));
+
 if(mysqli_num_rows($q)==0)
 {
 echo "0";	
@@ -34,8 +34,8 @@ else
 	while($data=mysqli_fetch_array($q))
 	{
 		$a = new user();
-		$a.setTitle($data['title']);
-		$a.setId($data['id']);
+		$a->setTitle($data['title']);
+		$a->setId($data['id']);
 	}
 	
 	array_push($resultArray,$a);
