7725879eb53f31e01d13c403f3d5db8aea30884c
Your Name <you@example.com>
2017-07-16 16:34:25 +0530

    doom

--- follow/login.php
+++ follow/login.php
@@ -25,9 +25,9 @@ $resultArray = array();
 
 $query = "select * from follow_profiles where userid = '$userid' and password = '$pass'";
 
-$qq = mysqli_query($link,$query);
+$the = mysqli_query($link,$query);
 $count = 0;
-	while($data=mysqli_fetch_array($qq))
+	while($data=mysqli_fetch_array($the))
 	{
 		$count++;
 		$a = new user();
