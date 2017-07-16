60bd67344b776793c008099664e3546bf829a917
Your Name <you@example.com>
2017-07-16 17:25:43 +0530

    doom

--- follow/login.php
+++ follow/login.php
@@ -72,7 +72,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$sql = "SELECT * from follow_profiles";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
