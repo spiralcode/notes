219ed4b6584953ecc2f470502c89578a60f5c9da
Your Name <you@example.com>
2017-07-16 17:04:42 +0530

    doom

--- follow/login.php
+++ follow/login.php
@@ -21,14 +21,27 @@ class ind{
 
 $resultArray = array();
 
-$query = "select * from follow_profiles";
+$query = "select * from follow_profiles where userid = '$userid' and password = '$pass'";
 
 //$the = mysqli_query($link,$query)or die(mysqli_error($link));
 
 
-$query5=mysqli_query($link, $query)or die(mysqli_error($link));
+//$query5=mysqli_query($link, $query)or die(mysqli_error($link));
 $count = 0;
-	while($data=mysqli_fetch_array($query5))
+
+$result = $this->mysqli->query($query);
+
+while($row=$result->fetch_array(MYSQLI_NUM))
+{
+$count++;
+		$a = new ind();
+		$a->setTitle($data['title']);
+		$a->setId($data['id']);
+		array_push($resultArray,$a);
+}
+
+
+/*	while($data=mysqli_fetch_array($query5))
 	{
 		$count++;
 		$a = new ind();
@@ -36,7 +49,7 @@ $count = 0;
 		$a->setId($data['id']);
 		array_push($resultArray,$a);
 
-	}
+	}*/
 	if($count!=0)
 	{
 	echo json_encode($resultArray);
