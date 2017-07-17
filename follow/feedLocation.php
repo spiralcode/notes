7102c7f0942099221e61f1dcc5eeec5daae56038
Your Name <you@example.com>
2017-07-17 21:35:51 +0530

    reshlap

--- follow/feedLocation.php
+++ follow/feedLocation.php
@@ -30,6 +30,7 @@ $accuracy = mysqli_real_escape_string($conn,$_POST['accuracy']);
 
 $sql = "update table follow_coords set coords = '$coords', speed = '$speed', accuracy = '$accuracy',time=now() where id = $id  ";
 $result = $conn->query($sql);
+echo "1";
 $conn->close();
 
 
