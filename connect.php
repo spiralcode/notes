e5db6fd91bafa01c6d8004453ac50a056478574c
Your Name <you@example.com>
2017-07-28 23:07:50 +0530

    connect.php relieved from localhost and remote mysql credential mismatch

--- connect.php
+++ connect.php
@@ -1,4 +1,41 @@
 <?php    
+error_reporting(E_ERROR | E_PARSE);
+
+$host = "127.10.171.2:3306";
+$user = "adminz8hImgI";
+$pass = 'rUP7aW8my2r6';
+$db = 'note';
+
+$conn = new mysqli($host,$user,$pass);
+$link=$conn;
+
+if ($conn->connect_error) {
+	
+$host = "localhost";
+$user = "root";
+$pass = 'dbase001';
+$db = 'notes';
+
+$conn = new mysqli($host,$user,$pass);
+$link=$conn;
+
+//die("Connection failed: " . $conn->connect_error);
+} 
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+
+}
+
+$db=mysqli_select_db($conn,$db);
+
+
+
+
+
+
+
+
+/*
 if($_SERVER['HTTP_HOST']!='localhost')
 {
    //Dont alter its remote credentials
@@ -21,4 +58,7 @@ if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
 } 
 $db=mysqli_select_db($conn,$db);
+
+*/
+
 ?>
--- way/connect.php
+++ way/connect.php
@@ -1,7 +1,44 @@
 <?php    
+error_reporting(E_ERROR | E_PARSE);
+
+$host = "127.10.171.2:3306";
+$user = "adminz8hImgI";
+$pass = 'rUP7aW8my2r6';
+$db = 'note';
+
+$conn = new mysqli($host,$user,$pass);
+$link=$conn;
+
+if ($conn->connect_error) {
+	
+$host = "localhost";
+$user = "root";
+$pass = 'dbase001';
+$db = 'notes';
+
+$conn = new mysqli($host,$user,$pass);
+$link=$conn;
+
+//die("Connection failed: " . $conn->connect_error);
+} 
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+
+}
+
+$db=mysqli_select_db($conn,$db);
+
+
+
+
+
+
+
+
+/*
 if($_SERVER['HTTP_HOST']!='localhost')
 {
-   //Dont alter it's remote 
+   //Dont alter its remote credentials
 $host = "127.10.171.2:3306";
 $user = "adminz8hImgI";
 $pass = 'rUP7aW8my2r6';
@@ -13,7 +50,7 @@ else
 $host = "localhost";
 $user = "root";
 $pass = 'dbase001';
-$db = 'note';	
+$db = 'notes';	
 }
 $conn = new mysqli($host,$user,$pass);
 $link=$conn;
@@ -21,4 +58,7 @@ if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
 } 
 $db=mysqli_select_db($conn,$db);
+
+*/
+
 ?>
